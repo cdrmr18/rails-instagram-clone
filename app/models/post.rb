6cafe631dde74d115bf905e2802fbcf1797c1c86
Kathy Tavia <42288986+cdrmr18@users.noreply.github.com>
2023-06-24 11:57:15 +0200

    add like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+  has_many :likes
+  has_many :likers, through: :likes, source: :user
+
   validates :photos, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..(5.megabytes) }
   validates :caption, presence: true
 end
