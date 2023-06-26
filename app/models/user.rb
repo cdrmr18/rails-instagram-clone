12c03d9ac05451bf72f6f2880b303f204ea8bf71
Kathy Tavia <42288986+cdrmr18@users.noreply.github.com>
2023-06-26 22:32:10 +0200

    add following model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,18 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+  # Not needed associations
+  # has_many :received_requests, class_name: "Follows", foreign_key: 'followed_id'
+  # has_many :sent_requests, class_name: "Follows", foreign_key: 'follower_id'
+  # has_many :follwing_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: 'follower_id'
+
+  has_many :followers, -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'followed_id'
+  has_many :accepted_received_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: 'followed_id'
+  has_many :accepted_sent_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'follower_id'
+
+  has_many :followers. through: :accepted_received_requests, source: :follower
+  has_many :followings. through: :accepted_sent_requests, source: :followed
+
 
   has_one_attached :avatar
 end
