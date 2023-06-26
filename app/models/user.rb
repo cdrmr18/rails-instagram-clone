ff67b0f24b6cb9134187f8ab8188f0ba25822d69
Kathy Tavia <42288986+cdrmr18@users.noreply.github.com>
2023-06-26 23:14:50 +0200

    add follow logic and helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,21 @@ class User < ApplicationRecord
   # has_many :sent_requests, class_name: "Follows", foreign_key: 'follower_id'
   # has_many :follwing_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: 'follower_id'
 
-  has_many :followers, -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'followed_id'
-  has_many :accepted_received_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: 'followed_id'
+  has_many :follow_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: 'followed_id'
+  has_many :accepted_received_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'followed_id'
   has_many :accepted_sent_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'follower_id'
 
-  has_many :followers. through: :accepted_received_requests, source: :follower
-  has_many :followings. through: :accepted_sent_requests, source: :followed
-
+  has_many :followers, through: :accepted_received_requests, source: :follower
+  has_many :followings, through: :accepted_sent_requests, source: :followed
 
   has_one_attached :avatar
+
+  def follow(user)
+    Follow.create(follower: self, followed: user)
+  end
+
+  def unfollow(user)
+    follow = self.accepted_sent_requests.find_by(followed: user)
+    follow&.destroy
+  end
 end
