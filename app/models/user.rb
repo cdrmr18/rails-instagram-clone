fac3f436b4186d19c709f492bd00e5235bf17636
Kathy Tavia <42288986+cdrmr18@users.noreply.github.com>
2023-06-29 12:09:10 +0200

    star follow requests list

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,24 +10,24 @@ class User < ApplicationRecord
 
   has_many :follow_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: 'followed_id'
   has_many :accepted_received_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'followed_id'
-  has_many :accepted_sent_requests -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'follower_id'
-
+  has_many :accepted_sent_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: 'follower_id'
   has_many :pending_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: 'follower_id'
 
   has_many :followers, through: :accepted_received_requests, source: :follower
   has_many :followings, through: :accepted_sent_requests, source: :followed
+  has_many :pending_followings, through: :pending_sent_requests, source: :followed
 
   has_one_attached :avatar
 
   def follow(user)
-    Follow.create(follower: self, followed: user)
+    Follow.create(follower: self, followed_id: user)
   end
 
   def unfollow(user)
-    self.accepted_sent_requests.find_by(followed: user)&.destroy
+    self.accepted_sent_requests.find_by(followed_id: user)&.destroy
   end
 
   def cancel_request(user)
-    self.pending_sent_requests.find_by(followed: user)&.destroy
+    self.pending_sent_requests.find_by(followed_id: user)&.destroy
   end
 end
